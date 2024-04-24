fc4f3b44bb461aac92aa8c896b69ca7f2a96b2c1
Kristian Hernandez <hernandez.cardenas10@gmail.com>
2024-04-24 14:18:37 -0600

    Initial project setup using Gradle

---
+++ kibobazar-backend/kibobazar-backend/src/main/java/com/kibobazar/kibobazarbackend/KibobazarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kibobazar.kibobazarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KibobazarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KibobazarBackendApplication.class, args);
+	}
+
+}
---
+++ kibobazar-backend/kibobazar-backend/src/test/java/com/kibobazar/kibobazarbackend/KibobazarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kibobazar.kibobazarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KibobazarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
