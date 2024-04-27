2b13b9281423fc8666e754054f0239ddd93cd6a2
Rodolfo Ruiz <rodolforuizordaz@gmail.com>
2024-04-26 23:59:34 -0600

    creando proyecto spring boot kibobazar con gradle

---
+++ kibobazar-api-rest/src/main/java/com/kibobazar/app/KibobazarApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.kibobazar.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KibobazarApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KibobazarApiRestApplication.class, args);
+	}
+
+}
